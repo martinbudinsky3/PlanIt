e6092376cbd06e52866e5f7ef876f53db7110676
Martin Budinský <martinbudinsky3@github.com>
2020-04-19 11:48:52 +0200

    comm

--- src/main/java/com/example/vavaplanit/Api/EventController.java
+++ src/main/java/com/example/vavaplanit/Api/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class EventController {
 
     @GetMapping("{userId}")
     public ResponseEntity getAllByUserId(int userId){
-        List<Event> eventList =eventService.getAllByUserId(userId);
+        List<Event> eventList = eventService.getAllByUserId(userId);
         return new ResponseEntity<>(eventList, HttpStatus.OK);
     }
 
