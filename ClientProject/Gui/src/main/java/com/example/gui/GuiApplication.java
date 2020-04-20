3c0587d8869f01c21308ee0b63ad841cf3745a20
Martin Budinský <martinbudinsky3@github.com>
2020-04-20 17:28:43 +0200

    pripravenie gui casti klientskeho projektu

--- ClientProject/Gui/src/main/java/com/example/gui/GuiApplication.java
+++ ClientProject/Gui/src/main/java/com/example/gui/GuiApplication.java
@@ -1,13 +1,27 @@
 package com.example.gui;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-@SpringBootApplication
-public class GuiApplication {
+public class GuiApplication extends Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GuiApplication.class, args);
+	public static void main(String[] args) {launch(args);}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setLocation(getClass().getClassLoader().getResource("fxml/PlanItMainWindow.fxml"));
+			AnchorPane rootPane = (AnchorPane) fxmlLoader.load();
+			Scene newScene = new Scene(rootPane, 600, 600);
+			primaryStage.setScene(newScene);
+			primaryStage.setTitle("PlanIt");
+			primaryStage.show();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
 	}
-
 }
---
+++ ClientProject/Gui/src/main/java/com/example/gui/controllers/PlanItMainWindowController.java
@@ -0,0 +1,5 @@
+package com.example.gui.controllers;
+
+public class PlanItMainWindowController {
+    
+}
