07da2dc91adfb5d2d012eec066121c25b220dd43
Martin Budinský <martinbudinsky3@github.com>
2020-08-27 12:38:01 +0200

    metoda setDaysOfWeek vo WeeklyRepetition - este neotestovana

--- ServerProject/src/main/java/com/example/vavaplanit/model/repetition/WeeklyRepetition.java
+++ ServerProject/src/main/java/com/example/vavaplanit/model/repetition/WeeklyRepetition.java
@@ -2,10 +2,26 @@ package com.example.vavaplanit.model.repetition;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WeeklyRepetition extends Repetition {
-    private List<DayOfWeek> daysOfWeek;
+    private final int DAYS_OF_WEEK_NUMBER = 7;
+    private final Map<Integer, DayOfWeek> intToDayOfWeekMap = new HashMap<Integer, DayOfWeek>(){
+        {
+            put(0, DayOfWeek.MONDAY);
+            put(1, DayOfWeek.TUESDAY);
+            put(2, DayOfWeek.WEDNESDAY);
+            put(3, DayOfWeek.THURSDAY);
+            put(4, DayOfWeek.FRIDAY);
+            put(5, DayOfWeek.SATURDAY);
+            put(6, DayOfWeek.SUNDAY);
+        }
+    };;
+
+    private List<DayOfWeek> daysOfWeek = new ArrayList<DayOfWeek>();
 
     public WeeklyRepetition() {
     }
@@ -23,6 +39,11 @@ public class WeeklyRepetition extends Repetition {
 
     public void setDaysOfWeek(Integer daysOfWeek) {
         // TODO extract days of week from int
+        for(int i = 0; i < DAYS_OF_WEEK_NUMBER; i++) {
+            if(((daysOfWeek >> i) & 1) == 1) {
+                this.daysOfWeek.add(intToDayOfWeekMap.get(i));
+            }
+        }
     }
 
     @Override
